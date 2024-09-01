cca4ed90f3c4d5df7c3bc0b3fd76ba6d4500c05d
Jeiso Guerrero <jei_son1516@hotmail.com>
2024-08-31 20:35:05 -0400

    fast api basics

---
+++ main.py
@@ -0,0 +1,56 @@
+from enum import Enum
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+# base path get operation
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+# path with parameters
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+"""
+When creating path operations, you can find situations where you have a fixed path.
+
+Like /users/me, let's say that it's to get data about the current user.
+
+And then you can also have a path /users/{user_id} to get data about a specific user by some user ID.
+
+Because path operations are evaluated in order, you need to make sure that the path for /users/me is declared before the one for /users/{user_id}:
+"""
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+# Enum classes
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
